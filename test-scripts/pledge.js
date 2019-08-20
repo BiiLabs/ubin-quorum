f0ccadf5947456fe537a33ff74f150bc6774de08
mushroom0117 <v860117@gmail.com>
2019-08-20 14:33:51 +0800

    Fix sha256 error

--- test-scripts/pledge.js
+++ test-scripts/pledge.js
@@ -6,7 +6,6 @@ var SGDz = artifacts.require("./SGDz.sol");
 var util = require('../util.js');
 var u = require('./test-utils.js');
 
-var sha256 = require('js-sha256').sha256;
 
 // set initial variables
 var bankIdx = process.argv[6];
@@ -16,11 +15,11 @@ var stashName = nodes[bankIdx].stashName;
 let j = u.searchNode(nodes, 'stashName', stashName);
 var consKey = [];
 
-if (!nodes[bankIdx].centralBank){
-    consKey.push(nodes[j].constKey);
-} else{
-    nodes = u.removeMe(nodes, 'MASGSGSG');
-    consKey = u.getValueFromAllNodes(nodes, 'constKey');
+if (!nodes[bankIdx].centralBank) {
+  consKey.push(nodes[j].constKey);
+} else {
+  nodes = u.removeMe(nodes, 'MASGSGSG');
+  consKey = u.getValueFromAllNodes(nodes, 'constKey');
 }
 
 console.log('constKey: ');
@@ -34,12 +33,14 @@ module.exports = (done) => {
   PaymentAgent.deployed().then((instance) => {
     paymentAgent = instance;
 
-    util.colorLog("Setting "+stashName+"'s stash balance to " + bal + "...", currentNetwork);
+    util.colorLog("Setting " + stashName + "'s stash balance to " + bal + "...", currentNetwork);
 
-    return paymentAgent.pledge('R'+Date.now(), stashName, bal, {gas: 1000000,
-								privateFor: consKey});
+    return paymentAgent.pledge('R' + Date.now(), stashName, bal, {
+      gas: 1000000,
+      privateFor: consKey
+    });
   }).then((result) => {
-    util.colorLog("\tmined!, block: "+result.receipt.blockNumber+", tx hash: "+result.tx, currentNetwork);
+    util.colorLog("\tmined!, block: " + result.receipt.blockNumber + ", tx hash: " + result.tx, currentNetwork);
     util.colorLog("");
 
     done();
