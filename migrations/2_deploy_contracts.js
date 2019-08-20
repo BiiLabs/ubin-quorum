c6fcaae8ce48dbfe9e21aa3f09e1e49039719e35
mushroom0117 <v860117@gmail.com>
2019-08-20 14:38:26 +0800

    Remove unused solidity contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,14 +4,8 @@ var nodes = JSON.parse(fs.readFileSync('../testnet.json', 'utf8'))['nodes'];
 var PaymentAgent = artifacts.require("./PaymentAgent.sol")
 var SGDz = artifacts.require("./SGDz.sol")
 
-var PaymentSeb = artifacts.require("./PaymentSeb.sol")
-var PaymentEmulator = artifacts.require("./PaymentEmulator.sol")
-
 module.exports = function(deployer) {
   deployer.deploy(SGDz);
   deployer.deploy(PaymentAgent, {privateFor: nodes.slice(1)});
-
-  deployer.deploy(PaymentSeb, {privateFor: nodes.slice(1)});
-  deployer.deploy(PaymentEmulator, {privateFor: nodes.slice(1)});
 };
 
